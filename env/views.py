b34a58da7e7771524903cf69acba7b56a37e71f2
adgarzon <angelduvan20@gmail.com>
2021-10-14 10:13:31 -0500

    duvan

--- env/views.py
+++ env/views.py
@@ -6,7 +6,7 @@ import yagmail
 main = blueprints.Blueprint('main', __name__)
 
 #login
-@main.route('/')
+@main.route('/',methods=['GET','POST'])
 def index():
  return render_template("login.html")
 
@@ -15,40 +15,29 @@ def index():
 def registro():
     return render_template("registroUsuario.html")
 
-
-#citas
-@main.route('/cita/')
-def cita():
-    return render_template('citas/gestionarCitas.html')
-
-@main.route('/cita/create/')
-def CrearCita():
-    return render_template('citas/crearCita.html')    
-
-
-#dashboard
-@main.route('/dashboard/')
-def dashboard():
-    return render_template('dashboard/index.html')
-
 #home
 @main.route('/inicio/')
 def inicio():
     return render_template('home/inicio.html')
 
+#citas
+@main.route('/gestioncita/',methods=['GET','POST','DELETE','PUT'])
+def cita():
+    return render_template('citas/gestionarCitas.html')
 
-@main.route('/listado/')
-def listado():
-    return render_template('pacientes/index.html',)
 
+#crear cita
+@main.route('/cita/create/',methods=['POST','GET'])
+def CrearCita():
+    return render_template('citas/crearCita.html')    
 
-@main.route('/cita/',methods=['POST'])
-def addCita():
+#dashboard
+@main.route('/dashboard/',methods=['GET'])
+def dashboard():
+    return render_template('dashboard/index.html')
 
-    request.json["hora"]
-    return render_template('cita/index.html')
-
-@main.route('/perfil/')
+#perfil
+@main.route('/perfil/',methods=['GET','POST','PUT'])
 def perfil():
     return render_template('perfil.html',)
 
